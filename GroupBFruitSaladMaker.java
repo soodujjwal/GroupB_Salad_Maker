cd861c67717c4c3f6f020b8d031787be81c717ae
Jay Mistry <90444018+Cipher-935@users.noreply.github.com>
2021-10-13 14:34:14 +0530

    Group B Salad Maker

---
+++ GroupBFruitSaladMaker.java
@@ -0,0 +1,26 @@
+package Assignment_4;
+
+import java.util.Scanner;
+
+public class GroupBFruitSaladMaker {
+
+	public static void main(String[] args) {
+		
+	  Scanner input = new Scanner(System.in);
+	  
+	  System.out.println("Enter how many grams of pineapple you want: ");
+	  double pineapple = input.nextDouble();
+	  System.out.println("Enter how many grams of cherry you want: ");
+	  double cherry = input.nextDouble();
+	  System.out.println("Enter how many grams of apple you want");
+	  double apple = input.nextDouble();
+	  System.out.println("Enter how many grams of dragonfruit you want.");
+	  double Dragonfruit = input.nextDouble();
+	  
+	  
+	  
+	  
+
+	}
+
+}
