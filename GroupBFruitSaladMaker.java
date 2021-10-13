68532a9c18db2723cdf687d02f1c4c7f4d4d9d17
soodujjwal <90448948+soodujjwal@users.noreply.github.com>
2021-10-13 14:51:03 +0530

    Update GroupBFruitSaladMaker.java

--- GroupBFruitSaladMaker.java
+++ GroupBFruitSaladMaker.java
@@ -16,9 +16,45 @@ public class GroupBFruitSaladMaker {
 	  double apple = input.nextDouble();
 	  System.out.println("Enter how many grams of dragonfruit you want.");
 	  double Dragonfruit = input.nextDouble();
-	  
-	  
-	  
+		
+		
+		
+ if(PineApple <= 0){
+	show = "Cherry, DragonFruit, and Banana\n";
+	if(Cherry <= 0){
+		show = "DragonFruit, and Banana\n";
+		if(DragonFruit <= 0){
+			show = "Banana\n";
+			if(Apple <= 0){
+				show = "Nothing Selected :(\n";
+			}
+		}
+	}
+}
+		else{
+			show = "PineApple, Cherry, DragonFruit, and Banana\n";
+			if(Cherry <= 0){
+				show = "PineApple, DragonFruit, and Banana\n";
+			}
+			else{
+				show = "PineApple, Cherry, DragonFruit, and Banana\n";
+				if(DragonFruit <= 0){
+					show = "PineApple, Cherry, and Banana\n";
+				}
+				else{
+					show = "PineApple, Cherry, DragonFruit, and Banana\n";
+					if(Apple <= 0){
+						show = "PineApple, Cherry, and DragonFruit\n";
+					}
+					else{
+						show = "PineApple, Cherry, DragonFruit, and Apple\n";
+					}
+				}
+			}
+		}
+		
+
+
 	  
 
 	}
