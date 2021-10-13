843c22adeabcaf683e75a3a3499a2b092f9b10e6
Vinit Jangir <90442953+vinitjangir18@users.noreply.github.com>
2021-10-13 16:25:05 +0530

    Completed project.

--- GroupBFruitSaladMaker.java
+++ GroupBFruitSaladMaker.java
@@ -10,66 +10,66 @@ package Assignment_4;
 
 import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class TeamB_FruitSalad{
+	public static void main(String[] args){
 
-	FruitIngredients object = new FruitIngredients();
-	FruitIngredients object = new FruitIngredients(); //Class object created.
-
-	Scanner input = new Scanner(System.in);
-	Scanner input = new Scanner(System.in); //Scanner object created.
-
-	System.out.println("Enter how many grams of pineapple you want: ");
-	double pineapple = input.nextDouble();
-		public static void main(String[] args) {
-
-	/* Nested if else created.*/
- 	if(pineapple <= 0){
-		show = "Cherry, DragonFruit, and Banana\n";
-		show = "Cherry, DragonFruit, and Apple\n";
-		if(cherry <= 0){
-			show = "DragonFruit, and Banana\n";
-			show = "DragonFruit, and Apple\n";
-			if(dragonfruit <= 0){
-				show = "Banana\n";
-				show = "Apple\n";
-				if(apple <= 0){
-					show = "Nothing Selected :(\n";
-				}
-			}
-		}
-	}
-	else{
-		show = "PineApple, Cherry, DragonFruit, and Banana\n";
-		show = "PineApple, Cherry, DragonFruit, and Apple\n";
-		if(cherry <= 0){
-			show = "PineApple, DragonFruit, and Banana\n";
-			show = "PineApple, DragonFruit, and Apple\n";
-		}
-		else{
-			show = "PineApple, Cherry, DragonFruit, and Banana\n";
-			show = "PineApple, Cherry, DragonFruit, and Apple\n";
-			if(dragonfruit <= 0){
-				show = "PineApple, Cherry, and Banana\n";
-				show = "PineApple, Cherry, and Apple\n";
-			}
-			else{
-				show = "PineApple, Cherry, DragonFruit, and Banana\n";
-				show = "PineApple, Cherry, DragonFruit, and Apple\n";
-				if(apple <= 0){
-					show = "PineApple, Cherry, and DragonFruit\n";
-				}
-@@ -58,7 +67,11 @@ public static void main(String[] args) {
-	}
-
-	object.setWeigth(sum); // setter function called for weight.
-        object.setIngredients(show); // setter function called for ingredients.	
-        object.setIngredients(show); // setter function called for ingredients.
-
-	System.out.print("Thank you for your order! Your fruit salad is ready.");
-        System.out.printf("Total weight of your salad is %.2fg, Ingredients: %s",object.getWeigth(), object.getIngredients());
-	scan.close();
-
-	}
+            FruitIngredients object = new FruitIngredients(); //Class object created.
+        
+            Scanner input = new Scanner(System.in); //Scanner object created.
+        
+            System.out.println("Welcome to Team B's Fruit Salad Maker!");
+            System.out.println("Enter how many grams of PineApple you want: ");
+            double pineapple = input.nextDouble();
+            System.out.println("Enter how many grams of Cherry you want: ");
+            double cherry = input.nextDouble();
+            System.out.println("Enter how many grams of DragonFruit you want: ");
+            double dragonfruit = input.nextDouble();
+            System.out.println("Enter how many grams of Apple you want: ");
+            double apple = input.nextDouble();
+        
+            String show = "";
+            double sum = pineapple + cherry + dragonfruit + apple;
+            
+            /* Nested if else created.*/
+             if(pineapple <= 0){
+                show = "Cherry, DragonFruit, and Apple\n";
+                if(cherry <= 0){
+                    show = "DragonFruit, and Apple\n";
+                    if(dragonfruit <= 0){
+                        show = "Apple\n";
+                        if(apple <= 0){
+                            show = "Nothing Selected :(\n";
+                        }
+                    }
+                }
+            }
+            else{
+                show = "PineApple, Cherry, DragonFruit, and Apple\n";
+                if(cherry <= 0){
+                    show = "PineApple, DragonFruit, and Apple\n";
+                }
+                else{
+                    show = "PineApple, Cherry, DragonFruit, and Apple\n";
+                    if(dragonfruit <= 0){
+                        show = "PineApple, Cherry, and Apple\n";
+                    }
+                    else{
+                        show = "PineApple, Cherry, DragonFruit, and Apple\n";
+                        if(apple <= 0){
+                            show = "PineApple, Cherry, and DragonFruit\n";
+                        }
+                    }
+        
+            }
+        }
+            object.setWeigth(sum); // setter function called for weight.
+            object.setIngredients(show); // setter function called for ingredients.
+        
+            System.out.print("Thank you for your order! Your fruit salad is ready.");
+            System.out.printf("Total weight of your salad is %.2fg, Ingredients: %s",object.getWeigth(), object.getIngredients());
+            input.close();
+        
+    }
 }
 class FruitIngredients{
     private double weigth; // private variable
